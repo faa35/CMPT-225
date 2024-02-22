f5739bc235fcc1cefd71aba395c253e2ffc1a5cb
Fardin <faa35@sfu.ca>
2024-02-22 05:29:06 -0800

    2

--- Selection_sort/Selection_sort.cpp
+++ Selection_sort/Selection_sort.cpp
@@ -11,6 +11,20 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+// // if we don't use std::swap
+// void selectionSort(int arr[], int n) {
+//     for (int i = 0; i < n - 1; i++) {
+//         int minIndex = i; // Corrected initialization
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[minIndex]) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[minIndex];
+//                 arr[minIndex] = temp;
+//             }
+//         }
+//     }
+// }
+
 int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -31,3 +45,7 @@ int main() {
 
     return 0;
 }
+
+// Output:
+// Original array: 64 25 12 22 11
+// Sorted array: 11 12 22 25 64
